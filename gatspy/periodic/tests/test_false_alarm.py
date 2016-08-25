0752b4c181265047107f504177715ba0792cc1a3
Eric <ebellm@gmail.com>
2016-08-25 12:27:38 -0700

    fix syntax error in test_false_alarm.py

--- gatspy/periodic/tests/test_false_alarm.py
+++ gatspy/periodic/tests/test_false_alarm.py
@@ -6,7 +6,7 @@ from numpy.testing import assert_allclose, assert_equal, assert_almost_equal
 
 from .. import LombScargle
 
-def test_lombscargle_fap(){
+def test_lombscargle_fap():
     t = np.linspace(0, 100, 1000)
     np.random.seed(4)
     y = np.random.normal(0, 1.0, t.size)
@@ -18,4 +18,3 @@ def test_lombscargle_fap(){
     ls.optimizer.period_range = (1/fmax, t.max())
 
     assert_almost_equal(ls.false_alarm_max(), 0.17453, decimal=3)
-}
